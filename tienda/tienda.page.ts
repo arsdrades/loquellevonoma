c0f28dfe9614fb98a2b716096021a68fdc80aeaf
Ariel Salas Andrades <101760081+arsdrades@users.noreply.github.com>
2022-10-26 12:11:22 -0300

    Add files via upload

---
+++ tienda/tienda.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+@Component({
+  selector: 'app-tienda',
+  templateUrl: './tienda.page.html',
+  styleUrls: ['./tienda.page.scss'],
+})
+
+export class TiendaPage{
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+}
